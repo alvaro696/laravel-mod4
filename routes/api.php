d7c25facdd287e7deb668729e30a93bece2ac868
alvaro696 <alvaro00.am@outlook.com>
2025-03-11 21:05:57 -0400

    primer push

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Ejemplo de ruta API
+Route::get('/test', function () {
+    return response()->json(['message' => 'Hello, API!']);
+});
